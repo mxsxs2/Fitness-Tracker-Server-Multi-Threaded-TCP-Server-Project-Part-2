8e041e7be9e5e5c7f91f819c349f7e7855c8958a
Mxsxs2 <>
2018-01-03 17:43:39 +0000

    Implement user, record and basic user register and login

--- src/ie/gmit/os/Runner.java
+++ src/ie/gmit/os/Runner.java
@@ -4,22 +4,8 @@ public class Runner {
 	public static void main(String[] args) {
 		//Create a new server and start it
 		Server s=new Server();
-		//s.runServer();
+		s.runServer();
 		
-		UserDatabase ud =new UserDatabase();
-		
-		User user = new User("1000201R","Krisztian","Address",25,172,72);
-		user.setPassword("Krisz");
-		
-		System.out.println("registered"+ud.isUserRegistered("1000201R"));
-		System.out.println("login"+ud.isValidUserCredetials(user));
-		try {
-			System.out.println("register"+ud.registerUser(user));
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		System.out.println("registered"+ud.isUserRegistered("1000201R"));
-		System.out.println("login"+ud.isValidUserCredetials(user));
 		
 		
 		
